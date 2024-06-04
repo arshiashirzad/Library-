9c0c640f9f3d6442321a5354c749ee36f4b93367
arshiashirzad <sirzad383@gmail.com>
2024-06-04 15:50:37 +0330

    Added Member.cpp and Member.h

---
+++ Member.cpp
@@ -0,0 +1,56 @@
+#include "Member.h"
+
+Member::Member(string name, string id, string memberId)
+        : Person(name, id), memberId(memberId), bookCount(0) {}
+
+Member::Member(const Member& other)
+        : Person(other), memberId(other.memberId), bookCount(other.bookCount) {
+    for (int i = 0; i < bookCount; ++i) {
+        books[i] = other.books[i];
+    }
+}
+
+string Member::getMemberId() const { return memberId; }
+void Member::setMemberId(const string& memberId) { this->memberId = memberId; }
+
+void Member::borrowBook(Book* book) {
+    if (bookCount < 10) {
+        books[bookCount++] = book;
+    }
+}
+
+void Member::returnBook(Book* book) {
+    for (int i = 0; i < bookCount; ++i) {
+        if (books[i] == book) {
+            for (int j = i; j < bookCount - 1; ++j) {
+                books[j] = books[j + 1];
+            }
+            bookCount--;
+            break;
+        }
+    }
+}
+
+bool Member::isBookBorrowed(Book* book) {
+    for (int i = 0; i < bookCount; ++i) {
+        if (books[i] == book) {
+            return true;
+        }
+    }
+    return false;
+}
+
+ostream& operator<<(ostream& os, const Member& member) {
+    os << "Member [Name: " << member.name << ", ID: " << member.id << ", MemberID: " << member.memberId << "]";
+    return os;
+}
+
+istream& operator>>(istream& is, Member& member) {
+    cout << "Enter name: ";
+    is >> member.name;
+    cout << "Enter ID: ";
+    is >> member.id;
+    cout << "Enter member ID: ";
+    is >> member.memberId;
+    return is;
+}
---
+++ Member.h
@@ -0,0 +1,28 @@
+#ifndef MEMBER_H
+#define MEMBER_H
+
+#include "Person.h"
+#include "Book.h"
+
+class Member : public Person {
+private:
+    string memberId;
+    Book* books[10];
+    int bookCount;
+
+public:
+    Member(string name, string id, string memberId);
+    Member(const Member& other);
+
+    string getMemberId() const;
+    void setMemberId(const string& memberId);
+
+    void borrowBook(Book* book);
+    void returnBook(Book* book);
+    bool isBookBorrowed(Book* book);
+
+    friend ostream& operator<<(ostream& os, const Member& member);
+    friend istream& operator>>(istream& is, Member& member);
+};
+
+#endif
